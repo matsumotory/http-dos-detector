dd1e68243c663041fe3fa6bc89fedecd66c1d6b7
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-06-23 23:10:00 +0900

    Refactor code

--- dos_detector/dos_detector_init.rb
+++ dos_detector/dos_detector_init.rb
@@ -7,14 +7,20 @@ class DosDetector
     @config = config
     @now = Time.now.to_i
     @counter_key = config[:counter_key].to_s
-    magic_str = config[:magic_str].to_s
-    @counter_key_time = "#{@counter_key}_#{magic_str}_time"
+    @counter_key_time = "#{@counter_key}_#{config[:magic_str]}_time"
+    @data = self._analyze
+
+    self._detect @data
   end
 
   def analyze
+    @data
+  end
+
+  def _analyze
     unless @counter_key.nil?
       cnt = @cache[@counter_key]
-      cnt = cnt.to_i + 1
+      cnt = cnt.to_i
 
       # counter time when initialized counter
       prev = @cache[@counter_key_time].to_i
@@ -29,16 +35,46 @@ class DosDetector
   end
 
   def init_or_update_cache data
-    @cache[@counter_key] = data[:counter].to_s
+    @cache[@counter_key] = 1.to_s if data[:counter] == 0
     @cache[@counter_key_time] = @now.to_s if data[:time_diff] == 0
   end
 
   def update_cache counter, date
-    @cache[@counter_key] = counter.to_s
+    @cache[@counter_key] = (counter + 1).to_s
     @cache[@counter_key_time] =  date.to_s
   end
 
-  def detect? data = nil
+  def detect? data=nil
+ 
+    # run anlyze when data is nothing
+    data = self.analyze if data.nil?
+    return false if data.nil?
+
+    thr = @config[:threshold_counter]
+    thr_time = @config[:threshold_time]
+    expire = @config[:expire_time]
+    behind = @config[:behind_counter]
+    cnt = data[:counter]
+    diff = data[:time_diff]
+
+    if cnt >= thr
+      if 0 <= diff && diff < thr_time
+        true
+      else
+        false
+      end
+    elsif cnt < 0
+      if diff > expire
+        false
+      else
+        true
+      end
+    else
+      false
+    end
+  end
+
+  def _detect data=nil
 
     # run anlyze when data is nothing
     data = self.analyze if data.nil?
@@ -54,22 +90,15 @@ class DosDetector
     diff = data[:time_diff]
 
     if cnt >= thr
-      if 0 < diff && diff < thr_time
+      if 0 <= diff && diff < thr_time
         self.update_cache behind, @now
-        true
       else
         self.update_cache 0, @now
-        false
       end
-    elsif cnt < 0
-      if diff > expire
-        self.update_cache 0, @now
-        false
-      else
-        true
-      end
+    elsif cnt < 0 && diff > expire
+      self.update_cache 0, @now
     else
-      false
+      self.update_cache cnt, (@now - diff)
     end
   end
 
