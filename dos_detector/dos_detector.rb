b46718e48652177b0f4582b2a63ca95f27af30ea
MATSUMOTO Ryosuke <matsu1229@gmail.com>
2015-06-26 17:14:27 +0900

    change parameters for detect scripts by default

--- dos_detector/dos_detector.rb
+++ dos_detector/dos_detector.rb
@@ -2,28 +2,28 @@ Server = get_server_class
 r = Server::Request.new
 cache = Userdata.new.shared_cache
 global_mutex = Userdata.new.shared_mutex
+host = r.hostname
 
 config = {
   # dos counter by key
-  :counter_key => r.hostname,
+  :counter_key => host,
 
   # judging dos access when the number of counter is between :behind_counter and 0
-  :behind_counter => -2000,
+  :behind_counter => -500,
 
   # set behind counter when the number of counter is over :threshold_counter
   # in :threshold_time sec
-  :threshold_counter => 10000,
-  :threshold_time => 5,
+  :threshold_counter => 100,
+  :threshold_time => 1,
 
   # expire dos counter and initialize counter even
   # if the number of counter is between :behind_counter and 0
-  :expire_time => 10,
+  :expire_time => 5,
 }
 
-host = r.hostname
-dos = DosDetector.new r, cache, config
-
+# process-shared lock
 global_mutex.try_lock_loop do
+  dos = DosDetector.new r, cache, config
   begin
     Server.return Server::HTTP_SERVICE_UNAVAILABLE if dos.detect?
   rescue => e
